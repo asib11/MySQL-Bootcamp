025797759d9477a691dee7e57cb3f3d3c23313e8
asib ahmed <ahmedasib1@gmail.com>
2024-01-14 12:58:13 +0600

    complete strirng

--- string.sql
+++ string.sql
@@ -13,3 +13,16 @@ select replace(title, ' ', '_') from books;
 select char_length(title) as len, title from books;
 -- upper= ucase and lower= lcase
 select concat('I LOVE ', ucase(title), ' !!!') from books;
+
+-- exercise 
+select reverse(ucase('Why does my cat look at me with such hatred?'));
+select replace(concat('I', ' ', 'like',' ', 'cats'),' ','_');
+select replace(title,' ','->') as title from books;
+select author_lname as forwards, reverse(author_lname) as backwards from books;
+select ucase(concat(author_fname,' ', author_lname)) from books;
+select concat(title,' was released in ', released_year ) as blurb from books;
+desc books;
+select title, char_length(title) as 'character count' from books;
+select concat(substr(title,1,10),'...') as 'short title',
+ concat(author_lname,',', author_fname) as author,
+ concat(stock_quantity, ' in stock') as quantity from books;
