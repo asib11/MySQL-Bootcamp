1cf002de6815adbb44bbe12fee1545aa7ec79df8
asib ahmed <ahmedasib1@gmail.com>
2024-01-22 12:48:20 +0600

    add logical and comparision

--- string.sql
+++ string.sql
@@ -131,3 +131,61 @@ create table tweets (content varchar(100), username varchar(20),
 create_at timestamp default now());
 insert into tweets (content, username) values('my first tweet','asib');
 select * from tweets;
+
+-- logical and comparison
+-- not equal !=
+select author_fname, title from books where released_year != 2017;
+-- not like 
+select title from books where title not like '%a%';
+-- greater than
+select 40> 20; -- its returned actually boolean value, bt sql return 0 or 1
+-- between
+select title from books where released_year >= 2004 and released_year<= 2014; -- without between operator
+select title from books where released_year between 2004 and 2014;
+select title from books where released_year not between 2004 and 2014;
+select * from birthinfo where birthtime between cast('4:00:00' as time) and cast('6:00:00' as time); -- same
+select * from birthinfo where hour(birthtime) between 4 and 6; -- same
+
+-- exercise
+SELECT * FROM books WHERE released_year < 1980;
+
+SELECT * FROM books 
+WHERE author_lname = 'Eggers'
+OR author_lname = 'Chabon';
+
+SELECT * FROM books
+WHERE author_lname = 'Lahiri'
+AND released_year > 2000;
+
+SELECT * FROM books
+WHERE pages >= 100 
+AND pages <= 200;
+
+SELECT * FROM books
+WHERE pages BETWEEN 100 and 200;
+
+SELECT title, author_lname FROM books
+WHERE author_lname LIKE 'C%'
+OR author_lname LIKE 'S%';
+
+SELECT title, author_lname
+FROM books WHERE SUBSTR(author_lname, 1, 1) in ('C', 'S');
+
+SELECT title, author_lname,
+CASE
+    WHEN title LIKE '%stories%' THEN 'Short Stories'
+    WHEN title = 'Just Kids' THEN 'Memoir' 
+    WHEN title = 'A Heartbreaking Work of Staggering Genius' THEN 'Memior'
+    ELSE 'Novel'
+END AS type
+FROM books;
+
+
+SELECT author_fname, author_lname,
+	CASE
+        WHEN COUNT(*) = 1 THEN '1 book'
+        ELSE CONCAT(COUNT(*), ' books')
+	END AS count
+FROM books
+WHERE author_lname IS NOT NULL
+GROUP BY author_fname, author_lname;
